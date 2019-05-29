5e2d773c5cdd86baa6ea2e6bb902ec123bc4ec69
Drake Costa <drake@saeris.io>
2019-05-29 14:43:58 -0700

    ✨ Add Parcel + VuePress Icons, Fix Inferno + Rollup Icons
    Fixed some minor typos in the Rollup Icon. Fixed the vertical alignment of the Inferno Icon.
    
    Added Parcel and VuePress Icons.
    
    Updated SCGIconProps interface to use React.FunctionComponent

--- packages/template-icons/src/types.ts
+++ packages/template-icons/src/types.ts
@@ -1,7 +1,7 @@
 import React from "react";
 
 export interface SVGIconProps
-  extends React.Component<React.HTMLAttributes<SVGElement>> {
+  extends React.FunctionComponent<React.HTMLAttributes<SVGElement>> {
   width?: number;
   height?: number;
   className?: string;
